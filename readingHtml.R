38aa2233ffbd0c0eec3afb0b0fda76bc2d8beb16
Phyyu <umphyyu@hotmail.com>
2015-07-16 12:56:01 -0700

    add readingHtml.R

---
+++ readingHtml.R
@@ -0,0 +1,7 @@
+library(XML)
+fileUrl <-"http://espn.go.com/nfl/team/_/name/bal/baltimore-ravens"
+
+doc <- htmlTreeParse(fileUrl, useInternalNodes = TRUE)
+
+##scores <- xpathSApply(doc,"//li@class='score']",xmlValue)
+teams <- xpathSApply(doc,"//li[@class='team-name']",xmlValue)
