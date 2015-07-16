61308012c69dc4548b44d8ca9a55217a5f738597
Phyyu <umphyyu@hotmail.com>
2015-07-16 11:49:15 -0700

    Add new readingXML.R

---
+++ readingXML.R
@@ -0,0 +1,11 @@
+library(XML)
+fileUrl <- "http://www.w3schools.com/xml/simple.xml"
+doc <- xmlTreeParse(fileUrl, useInternalNodes=TRUE)
+rootNode <-xmlRoot(doc)
+xmlName(rootNode)
+
+##rootNode[[1]]
+
+xpathSApply(rootNode,"//name",xmlValue)
+
+
